fb4a95c19102d6c6964aa8ff7573a2486a38de93
sagming40 <bnmk102@gmail.com>
2026-05-24 19:18:52 +0900

    feat: PlannerItem 추상 클래스 및 StudyTask 상속 구현
    기말 프로젝트 요구사항 반영을 위해 Models 폴더 내 부모 추상 클래스(PlannerItem)를 설계하고, 이를 상속받는 구체 클래스(StudyTask) 구현. 다형성을 위한 추상 메서드 오버라이딩 및 문자열 보간 문법 적용.

---
+++ DevPomodoroPlanner/DevPomodoroPlanner/Models/PlannerItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevPomodoroPlanner.Models
+{
+    // abstract 키워드가 붙은 추상 클래스 (단독으로 객체 생성 불가, 오직 뼈대 역할)
+    public abstract class PlannerItem
+    {
+        // 공통 속성 정의
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        // 자식 클래스들이 각자 사정에 맞게 반드시 구현(재정의)해야 하는 추상 메서드
+        public abstract string GetTaskSummary();
+    }
+}
---
+++ DevPomodoroPlanner/DevPomodoroPlanner/Models/StudyTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevPomodoroPlanner.Models
+{
+    // : PlannerItem 을 붙여 부모 클래스 상속
+    public class StudyTask : PlannerItem
+    { 
+        // StudyTask만의 고유 속성 추가
+        public int SubjectId { get; set; }
+        public int TargetMinutes { get; set; }
+        public int ActualMinutes { get; set; }
+
+        // 부모가 물려준 추상 메서드를 override 키워드로 실제 구현
+        // 문자열 보간($"{ }") 사용
+        public override string GetTaskSummary()
+        {
+            return $"[{CreatedDate:yyyy-MM-dd}] {Title} - 목표: {TargetMinutes}분 / 실제 몰입: {ActualMinutes}분";
+        }
+    }
+}
