6ca60647ff40abe1d2c86403c2aa853fe382aa70
sagming40 <bnmk102@gmail.com>
2026-05-25 13:55:17 +0900

    layout: Dock 속성을 활용한 대시보드 3분할(좌/우/중앙) 패널 배치
    창 크기 조절 시 UI 깨짐 현상을 방지하기 위해 pnlLeft(Dock:Left), pnlRight(Dock:Right), pnlCenter(Dock:Fill) 구조를 도입함. 패널 간 시각적 구분을 위해 백그라운드 컬러를 톤다운(RGB 26, 26, 26)하여 계층적 다크모드 레이아웃을 확립함.

--- DevPomodoroPlanner/DevPomodoroPlanner/Form1.Designer.cs
+++ DevPomodoroPlanner/DevPomodoroPlanner/Form1.Designer.cs
@@ -28,7 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.pnlRight = new System.Windows.Forms.Panel();
+            this.pnlCenter = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(350, 673);
+            this.pnlLeft.TabIndex = 0;
+            // 
+            // pnlRight
+            // 
+            this.pnlRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.pnlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlRight.Location = new System.Drawing.Point(862, 0);
+            this.pnlRight.Name = "pnlRight";
+            this.pnlRight.Size = new System.Drawing.Size(400, 673);
+            this.pnlRight.TabIndex = 1;
+            // 
+            // pnlCenter
+            // 
+            this.pnlCenter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.pnlCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCenter.Location = new System.Drawing.Point(350, 0);
+            this.pnlCenter.Name = "pnlCenter";
+            this.pnlCenter.Size = new System.Drawing.Size(512, 673);
+            this.pnlCenter.TabIndex = 2;
             // 
             // Form1
             // 
@@ -36,6 +66,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.pnlCenter);
+            this.Controls.Add(this.pnlRight);
+            this.Controls.Add(this.pnlLeft);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(252)))));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
@@ -47,6 +80,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.Panel pnlRight;
+        private System.Windows.Forms.Panel pnlCenter;
     }
 }
 
